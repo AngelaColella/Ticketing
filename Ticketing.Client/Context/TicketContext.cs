0260e17309ef559b37af45d381a4fe25cd04a750
angela.colella <angela.colella@WINAPIUZIYVIF6L>
2020-12-01 09:42:21 +0100

    Add project files.

---
+++ Ticketing.Client/Context/TicketContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Ticketing.Client.Model;
+
+namespace Ticketing.Client.Context
+{
+    public class TicketContext : DbContext
+    {
+        DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer("connString");
+        }
+    }
+}
---
+++ Ticketing.Client/Model/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticketing.Client.Model
+{
+    public class Ticket
+    {
+        public int Id { get; set; } // per rispettare la convenzione della chiave primaria avrei potuto scrivere anche TicketId, ID, TicketID
+
+        public DateTime IssueDate { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+        public string Priority { get; set; }
+
+        public string State { get; set; }
+    }
+}
