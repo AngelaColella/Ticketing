17e7fc86a57fc88d2aec21576ff2b710882a4ca1
angela.colella <angela.colella@WINAPIUZIYVIF6L>
2020-12-02 16:06:37 +0100

    New Branch Commit

--- Ticketing.Core/Model/Note.cs
+++ Ticketing.Core/Model/Note.cs
@@ -6,6 +6,8 @@ namespace Ticketing.Core.Model
 {
     public class Note
     {
+        // va tolto il costruttore con l'inizializzazione della lista vuota a causa dell'aggiornamento di EF
+        
         //public Note()
         //{
         //    Ticket = new Ticket();
